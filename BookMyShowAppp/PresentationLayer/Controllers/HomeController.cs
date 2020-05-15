7b1e02c03d357ce73810aaa2eb78b7cd028ad364
nupurbhatt1998 <nupurbhatt2014@gmail.com>
2020-05-15 15:09:34 +0530

    bookmyshow

---
+++ BookMyShowAppp/PresentationLayer/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationLayer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult MoviePage()
+        {
+            return View();
+        }
+        public ActionResult Martian()
+        { return View();
+        }
+        public ActionResult BookNow()
+        { return View();
+        }
+        public ActionResult BookSeats()
+        { return View(); }
+    }
+}
